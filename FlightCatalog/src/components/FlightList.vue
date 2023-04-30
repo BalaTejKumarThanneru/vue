56e5580bb8be565950a9ff354286dd4b2975c9ec
teja <tbala3@unh.newhaven.edu>
2023-04-30 01:17:20 -0400

    second commit

---
+++ FlightCatalog/src/components/FlightList.vue
@@ -0,0 +1,35 @@
+<template>
+   
+    <div v-for="flight of flights.FlightDetails" :key="flight.id">
+
+       
+        <SingleFlight :flight="flight" />
+
+    </div>
+    
+    
+</template>
+<script>
+import SingleFlight from "./SingleFlight.vue"
+
+export default {
+    name: 'FlightList',
+    props:{
+        flights: Array
+    },
+    components: {
+        SingleFlight
+    }
+    
+}
+</script>
+
+<style scoped>
+.bookbox{
+
+    background-color: bisque;
+    padding: 1em;
+    color: gray;
+    margin: auto;
+}
+</style>
---
+++ FlightCatalog/src/components/SingleFlight.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="flight-details">
+      <h2 class="title">{{ flight.name }}</h2>
+      <div class="details">
+        <div class="detail">
+          <h3 class="label">Speed</h3>
+          <p class="value">{{ flight.speed }}</p>
+        </div>
+        <div class="detail">
+          <h3 class="label">Year</h3>
+          <p class="value">{{ flight.year }}</p>
+        </div>
+        <div class="detail">
+          <h3 class="label">Capacity</h3>
+          <p class="value">{{ flight.capacity }}</p>
+        </div>
+        <div class="detail">
+          <h3 class="label">Condition</h3>
+          <p class="value">{{ flight.condition }}</p>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'SingleFlight',
+    props: {
+      flight: Array
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .flight-details {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 20px;
+  padding: 20px;
+  background-color: rgb(238, 156, 156);
+  border-radius: 5px;
+}
+
+.title {
+  font-size: 32px;
+  margin-bottom: 16px;
+}
+
+.details {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  width: 100%;
+}
+
+.detail {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 16px;
+}
+
+.label {
+  font-size: 16px;
+  margin-bottom: 8px;
+}
+
+.value {
+  font-size: 24px;
+  font-weight: bold;
+}
+
+  </style>
+  
